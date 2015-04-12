285da198c3e6759dc86cc19cab50d159f74e26e9
Aaron Greenwald <aaron@aarongreenwald.com>
2015-04-11 21:12:42 -0400

    Remove www and support trailing slashes
    All routes now redirect from no trailing
    slash to a trailing slash. I'd rather
    do the opposite, but I couldn't (quickly)
    find a way to support that with pyramid.
    I also removed the wwww from all links to
    github and other applications.

--- landing/__init__.py
+++ landing/__init__.py
@@ -1,19 +1,25 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
+from pyramid.response import Response
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_route('home', '/')
-    config.add_route('resume', '/resume')
-    config.add_route('projects', '/projects')  
+    config.add_route('resume', '/resume/')
+    config.add_route('projects', '/projects/')  
 
-    config.add_route('blog', '/blog')
-    config.add_route('blog-post', '/blog/{slug}')  
-    config.add_route('blog-tag', '/blog/tag/{tag}')
+    config.add_route('blog', '/blog/')
+    config.add_route('blog-post', '/blog/{slug}/')  
+    config.add_route('blog-tag', '/blog/tag/{tag}/')
 
     config.add_route('qr', '/qr')  
     config.add_static_view('/static', 'resources', cache_max_age = 0) 
+
+    def notfound(request):
+        return Response('Not Found', status='404 Not Found')
+
+    config.add_notfound_view(notfound, append_slash=True)
     config.scan()  
     
     return config.make_wsgi_app()    
