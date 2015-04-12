b53cd0988d081674ecf36a36e2c0e83643137230
Aaron Greenwald <aaron@aarongreenwald.com>
2015-04-11 23:24:27 -0400

    blog styling, enhancements, logging

--- landing/blog_post.py
+++ landing/blog_post.py
@@ -10,10 +10,9 @@ class BlogPost(object):
             with open('./blog/' + self.slug) as f:
                 self.title = f.readline()
                 self.date = f.readline()
-                self.tags = f.readline().strip().split(' ')
-                print(self.tags)
-                if len(args) and args[0]:
-                    self.content = f.read(300)
+                self.tags = f.readline().strip().split(' ')                
+                if len(args) and args[0]:                    
+                    self.content = f.read(400)[:400].rsplit(' ', 1)[0]            
                 else:
                     self.content = f.read()               
         except IOError as e:
