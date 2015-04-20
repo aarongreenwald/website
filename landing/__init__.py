f8f66c575196e54467b76fcee5321a789211eb4e
Aaron Greenwald <aaron@aarongreenwald.com>
2015-04-20 06:38:33 -0400

    remove trailing slash from all urls

--- landing/__init__.py
+++ landing/__init__.py
@@ -7,21 +7,21 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     
     config.add_route('home', '/')
-    config.add_route('resume', '/resume/')
-    config.add_route('projects', '/projects/')  
+    config.add_route('resume', '/resume')
+    config.add_route('projects', '/projects')  
 
-    config.add_route('blog', '/blog/')
-    config.add_route('blog-post', '/blog/{slug}/')  
-    config.add_route('blog-tag', '/blog/tag/{tag}/')
+    config.add_route('blog', '/blog')
+    config.add_route('blog-post', '/blog/{slug}')  
+    config.add_route('blog-tag', '/blog/tag/{tag}')
 
-    config.add_route('qr', '/qr/')
+    config.add_route('qr', '/qr')
 
     config.add_static_view('/static', 'resources', cache_max_age = 0) 
 
     def notfound(request):
         return Response('Not Found', status='404 Not Found')
 
-    config.add_notfound_view(notfound, append_slash=True)
+    config.add_notfound_view(notfound)
     
     config.scan()  
     return config.make_wsgi_app()    
