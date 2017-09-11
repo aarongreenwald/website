bed2985834633dd4b07a914c70932d32080db1ee
Aaron Greenwald <aarong@wix.com>
2017-09-11 12:47:09 +0300

    better logging

--- logger.js
+++ logger.js
@@ -1,7 +1,8 @@
 const ip2country = require('ip2country');
 const uap = require('ua-parser-js');
 const fs = require('fs');
-
+const {Buffer} = require('buffer');
+const fetch = require('node-fetch');
 
 const isBot = ({ua, path}) => {
   const botUA = [
@@ -14,6 +15,7 @@ const isBot = ({ua, path}) => {
     'wp-login',
     'wp-includes',
     'wp-content',
+    'wp-admin',
     'cgi-bin',
     'a2billing',
     '.well-known/',
@@ -22,27 +24,54 @@ const isBot = ({ua, path}) => {
   return botUA.some(x => ua.toLowerCase().includes(x.toLowerCase())) || botPaths.some(x => path.toLowerCase().includes(x.toLowerCase()));
 };
 
+const encode = string => new Buffer(string).toString('base64');
+
+let sendToMixPanel = function (event) {
+  event.token = process.env.MP_TOKEN;
+  if (event.token) {
+    const data = encode(JSON.stringify([{
+      event: 'request',
+      properties: event
+    }]));
+
+    fetch(`http://api.mixpanel.com/track/`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      body: `data=${data}`
+    });
+  }
+
+};
+
 module.exports = {
   log: req => {
     const ip = req.headers['x-forwarded-for'] || req.ip;
     const ua = req.headers['user-agent'];
     const {path, query, headers} = req;
-    const log = {
-      timestamp: (new Date()).getTime(),
+    const logData = {
+      time: (new Date()).getTime(), //must be named 'time' for MP
+      timestamp: new Date(), //formatted nicely for readability
       path,
       query,
       user_agent: uap(ua),
+      isBot: isBot({ua, path}),
       referrer: headers['referer'] || headers['referrer'],
-      ip,
+      ip, //special MP field,
+      ip_address: ip, //readability
       country: ip2country(ip)
     };
 
-    const logPath = isBot({ua, path}) ? process.env.BOT_LOGS : process.env.ACCESS_LOGS;
+    sendToMixPanel(logData);
+
+    const logPath = logData.isBot ? process.env.BOT_LOGS : process.env.ACCESS_LOGS;
     if (logPath) {
-      fs.appendFile(logPath, JSON.stringify(log) + '\n', err => { if (err) console.error(err) });
+      fs.appendFile(logPath, JSON.stringify(logData) + '\n', err => { if (err) console.error(err) });
     } else {
-      console.log(log);
+      console.log(logData);
     }
   },
   isBot
 };
+
